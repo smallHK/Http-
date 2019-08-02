94aa90cfbd8b6fed1a75cf14c3424fd6c3ca6b42
smallHK <30528205+smallHK@users.noreply.github.com>
2019-08-02 11:18:47 +0800

    创建http包
    重修代码结构

--- http/http.go
+++ http/http.go
@@ -1,4 +1,47 @@
 package http
 
+import "fmt"
+
+type HttpRequest struct {
+	StartLine
+	Header
+	Body
+}
+
+type Body struct {
+	bytes []byte
+}
+
+type Header struct {
+	count int
+	items []HeaderItem
+}
+
+func NewEmptyHeader() Header {
+	return Header{
+		count:0,
+		items:make([]HeaderItem, 10),
+	}
+}
+
+type HeaderItem struct {
+	key string
+	value string
+}
+
+func (p *HeaderItem) printStr() {
+	fmt.Println(p.key+"="+p.value)
+}
+
+func (p *HeaderItem) parseByte(bytes []byte) error {
+
+	return nil
+}
+
+type StartLine struct {
+	method string
+	requestTarget string
+	httpVersion string
+}
 
 
--- main.go
+++ main.go
@@ -4,52 +4,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"github.com/smallHK/HttpGoParser/http"
 )
 
 
-
-
-type HttpRequest struct {
-	StartLine
-	Header
-	Body
-}
-
-type Body struct {
-	bytes []byte
-}
-
-type Header struct {
-	count int
-	items []HeaderItem
-}
-
-func NewEmptyHeader() Header {
-	return Header{
-		count:0,
-		items:make([]HeaderItem, 10),
-	}
-}
-
-type HeaderItem struct {
-	key string
-	value string
-}
-
-func (p *HeaderItem) printStr() {
-	fmt.Println(p.key+"="+p.value)
-}
-
-func (p *HeaderItem) parseByte(bytes []byte) error {
-
-	return nil
-}
-
-type StartLine struct {
-	method string
-	requestTarget string
-	httpVersion string
-}
 
 /**
 解析起始行
